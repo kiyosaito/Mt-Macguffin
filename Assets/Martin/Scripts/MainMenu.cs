c971deee703e6b986c654483715eefaa2387824e
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-08-27 18:01:26 +1000

    Menu

---
+++ Assets/Martin/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    #region Options Menu
+    public void SetVolume()
+    {
+
+    }
+    #endregion
+    #region Main Menu
+    public void MainMenuPlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenuQuit()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
