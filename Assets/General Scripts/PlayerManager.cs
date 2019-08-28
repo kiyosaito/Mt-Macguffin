069d5196c260fc795c9b7fcb33c1940cbef9fb90
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-08-28 11:53:36 +1000

    Update PlayerManager.cs

--- Assets/General Scripts/PlayerManager.cs
+++ Assets/General Scripts/PlayerManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    #region Martin's Code
+
+    #endregion
 }
