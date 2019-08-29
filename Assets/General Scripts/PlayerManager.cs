202906163ad86c306689bd2ba6cf092b9bd7b900
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-08-29 16:13:21 +1000

    Player and respawn script
    -player script variables and references added
    -checkpoint script created.

--- Assets/General Scripts/PlayerManager.cs
+++ Assets/General Scripts/PlayerManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    
+    #region General Variables/References
+
+    public int maxHealth;
+    public int curHealth;
+    public GameObject thePlayer;
+
+    #endregion
     void Start()
     {
         
@@ -16,6 +22,34 @@ public class PlayerManager : MonoBehaviour
     }
 
     #region Martin's Code
+
+    #region Variables/References
+    [Header("Invincibility")]
+    public float invincibilityLength;
+    private float invincibilityCount;
+
+    [Header("Respawn")]
+    private bool isRespawning;
+    private Vector3 respawnPoint;
+    public float respawnLength;
+
+    #endregion
+
+    #region Respawning
+
+    public IEnumerator RespawnCo(){
+        isRespawning = true;
+        thePlayer.gameObject.SetActive(false);
+        Instantiate(thePlayer.transform.position, thePlayer.transform.rotation);
+    }
+
+
+    #endregion
+
+
+    #endregion
+
+    #region Andrew's Code
 
     #endregion
 }
