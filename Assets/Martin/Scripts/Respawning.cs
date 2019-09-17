97315ac1c20eb58122fd534d19d4243c9f0dc199
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-09-17 16:35:19 +1000

    update

---
+++ Assets/Martin/Scripts/Respawning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawning : MonoBehaviour
+{
+    public GameObject player;
+    private bool _isRespawning;
+    private Vector3 _respawnPoint;
+    void Start()
+    {
+        _respawnPoint = player.transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Respawn()
+    {
+        player.transform.position = _respawnPoint;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "KillZone")
+        {
+            Respawn();
+        }
+    }
+}
