24e2165d638ad7e3f8cc7dbdb5bac7cddefa9324
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-09-19 15:56:54 +1000

    respawn,delay and checkpoints

---
+++ Assets/Martin/Scripts/KillZone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : Respawning
+{
+    public Player playerScript;
+    private void Awake()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        DisablePlayer();
+        Invoke("RespawnPlayer", 1);
+        Invoke("EnablePlayer", 2);
+    }
+    void RespawnPlayer()
+    {
+        player.transform.position = playerGhost.transform.position;
+    }
+    void DisablePlayer()
+    {
+        playerScript.enabled = false;
+    }
+    void EnablePlayer()
+    {
+        playerScript.enabled = true;
+    }
+}
--- Assets/Martin/Scripts/Respawning.cs
+++ Assets/Martin/Scripts/Respawning.cs
@@ -4,28 +4,14 @@ using UnityEngine;
 
 public class Respawning : MonoBehaviour
 {
+
     public GameObject player;
-    private bool _isRespawning;
-    private Vector3 _respawnPoint;
-    void Start()
-    {
-        _respawnPoint = player.transform.position;
-    }
+    public GameObject playerGhost;
 
-    void Update()
+    private void Start()
     {
-        
-    }
-    public void Respawn()
-    {
-        player.transform.position = _respawnPoint;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "KillZone")
-        {
-            Respawn();
-        }
+        playerGhost = GameObject.FindGameObjectWithTag("RespawnPoint");
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerGhost.transform.position = player.transform.position;
     }
 }
